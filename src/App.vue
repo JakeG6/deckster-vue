d8d4fc717557b81592502a58c0c6036e90ebf395
Jacob <jakeguss@gmail.com>
2019-08-20 14:13:59 -0600

    working on new deck form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
+    <NewDeckForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+import NewDeckForm from './components/NewDeckForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home,
+    NewDeckForm
   }
 }
 </script>
---
+++ src/components/NewDeckForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>wow</p>
+        <textarea v-model="message" placeholder="add multiple lines"></textarea>
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'NewDeckForm',
+}
+</script>
